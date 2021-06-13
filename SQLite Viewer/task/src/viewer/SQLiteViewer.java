23409365f6c6603b878f27867834e2e67ccc5c41
Christian Jairo Sarmiento <55115040+jairo8925@users.noreply.github.com>
2021-06-13 15:59:59 -0700

    Initial setup of Swing GUI

--- SQLite Viewer/task/src/viewer/SQLiteViewer.java
+++ SQLite Viewer/task/src/viewer/SQLiteViewer.java
@@ -5,10 +5,28 @@ import javax.swing.*;
 public class SQLiteViewer extends JFrame {
 
     public SQLiteViewer() {
+        super("SQLite Viewer");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 900);
         setLayout(null);
         setResizable(false);
         setLocationRelativeTo(null);
+
+        initComponents();
+
+        setVisible(true);
     }
+
+    private void initComponents() {
+        JTextField nameTextField = new JTextField();
+        nameTextField.setName("FileNameTextField");
+        nameTextField.setBounds(25, 20, 500, 30);
+        add(nameTextField);
+
+        JButton openFileButton = new JButton("Open");
+        openFileButton.setName("OpenFileButton");
+        openFileButton.setBounds(550, 20, 110, 30);
+        add(openFileButton);
+    }
+
 }
